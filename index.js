1605327893fdcce6d08bfa7bc564b47b9560dca3
jeremyvw <jeremyvw30@gmail.com>
2019-10-30 21:30:39 +0700

    Coba animasi dalam kubus

--- index.js
+++ index.js
@@ -178,92 +178,104 @@
       gl.drawArrays(type, 0, n);
     }  
 
-    function drawShapes2(type,vertices,n){
-      var triangleVertexBufferObject = gl.createBuffer();
-      gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-  
-      var vPosition = gl.getAttribLocation(program2, 'vPosition');
-      var vColor = gl.getAttribLocation(program2, 'vColor');
     
-      gl.vertexAttribPointer(
-        vPosition,  // variabel yang memegang posisi attribute di shader
-        2,          // jumlah elemen per atribut
-        gl.FLOAT,   // tipe data atribut
-        gl.FALSE, 
-        5 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex 
-        0                                   // offset dari posisi elemen di array
-      );
-      gl.vertexAttribPointer(
-        vColor,
-        3,
-        gl.FLOAT,
-        gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT,
-        2 * Float32Array.BYTES_PER_ELEMENT
-      );
-      gl.enableVertexAttribArray(vPosition);
-      gl.enableVertexAttribArray(vColor);
-  
-      var vPosition = gl.getAttribLocation(program2, 'vPosition');
-      var vColor = gl.getAttribLocation(program2, 'vColor');
-      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-      gl.drawArrays(type, 0, n);
-    }  
-
-    var translation = gl.getUniformLocation(program, 'translation');
-    var translationVector = [0.0, -0.5, 0.0];
-    gl.uniform3fv(translation, translationVector);
-  
-    var thetaLocation = gl.getUniformLocation(program, 'theta');
-    var theta = 0.0; 
-
-  function render(){
-      // Bersihkan layar jadi hitam
-      gl.useProgram(program);
-      gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  
-      // Bersihkan buffernya canvas
-      gl.clear(gl.COLOR_BUFFER_BIT);
-
-      
-      theta += 0.0062;
-      gl.uniform1f(thetaLocation, theta);
-  
-      drawShapes(gl.LINES, linesVertices1, 2);
-      drawShapes(gl.LINES, linesVertices2, 2);
-      drawShapes(gl.LINES, linesVertices5, 2);
-      drawShapes(gl.LINES, circleVertices, 180);
-      drawShapes(gl.LINES, circleVertices1, 180);
-      drawShapes(gl.LINES, linesVertices8, 2);    
-      requestAnimationFrame(render);
-
-  }
-  render();
-
-    var melebar = 1;
-    var scaleXLocation = gl.getUniformLocation(program2, 'scaleX');
-    // var scaleYLocation = gl.getUniformLocation(program2, 'scaleY');
-    var scaleX = 1.0;
-    // var scaleY = 1.0;
+    var thetaLoc1 = gl.getUniformLocation(program2, 'theta1'); 
+    var transLoc1 = gl.getUniformLocation(program2, 'trans1');
+    var thetaA1 = [10, 20, 0];
+    var trans1 = [0, 0, 0]; 
+    var X1 = 0.0080;
+    var Y1 = 0.0090;
+    var Z1 = 0.0130;
       
 
   function render2(){
-    // Bersihkan layar jadi hitam
     gl.useProgram(program2);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  
+    if(trans1[0] >= 0.4*0.8 || trans1[0] <= -0.3*0.8 ){
+      X1 *= -1;
+    }
+    trans1[0] += X1;
 
-    if (scaleX >= 1) melebar = -1;
-    else if (scaleX <= -1) melebar = 1; 
-    scaleX += 0.0062 * melebar;
-    gl.uniform1f(scaleXLocation, scaleX);
-    // gl.uniform1f(scaleYLocation, scaleY);
+    if(trans1[1] >= 0.6*0.8 || trans1[1] <= -0.8*0.8 ){
+      Y1 *= -1;
+    }
+    trans1[1] += Y1;
 
+    if(trans1[2] >= 0.7*0.8 || trans1[2] <= -0.6*0.8){
+      Z1 *= -1;
+    }
+    trans1[2] += Z1;
+
+    gl.uniform3fv(transLoc1, trans1);
+    thetaA1[1] += 0.062;
+    gl.uniform3fv(thetaLoc1, thetaA1);  
+  // gl.uniform1f(scaleYLocation, scaleY);
+
+    //huruf b dengan isian
     drawShapes(gl.TRIANGLE_STRIP, line1, 3);
     drawShapes(gl.TRIANGLE_STRIP, line2, 3);
     drawShapes(gl.TRIANGLE_STRIP, circleVertices2, 360);
 
     requestAnimationFrame(render2);
+  } 
+  render2();
+
+function drawShapes3(type,vertices,n) {
+    var vertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
+
+    var vPosition = gl.getAttribLocation(program3, 'vPosition');
+    var vColor = gl.getAttribLocation(program3, 'vColor');
+
+    gl.vertexAttribPointer(
+      vPosition, //variabel pemegang posisi atribut di shader
+      3,          // jumlah elemen per atribut
+      gl.FLOAT,   // tipe data atribut
+      gl.FALSE,   
+      6 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex
+      0
+    );
+
+    gl.vertexAttribPointer(
+      vColor, 
+      3, 
+      gl.FLOAT, 
+      gl.FALSE, 
+      6 * Float32Array.BYTES_PER_ELEMENT, 
+      3 * Float32Array.BYTES_PER_ELEMENT
+    );
+    gl.enableVertexAttribArray(vPosition);
+    gl.enableVertexAttribArray(vColor);
+
+    var vPosition = gl.getAttribLocation(program3, 'vPosition');
+    var vColor = gl.getAttribLocation(program3, 'vColor');
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.drawArrays(type, 0, n);
+  }
+  
+  var thetaLocCube = gl.getUniformLocation(program3, 'theta');
+  
+  function render3()
+  {
+    gl.useProgram(program3);
+    var thetaCube = [10, 10, 0];
+    gl.uniform3fv(thetaLocCube, thetaCube);
+    drawShapes3(gl.LINES, cube , 24);
+
+    requestAnimationFrame(render3);
+  }
+  // render();
+  render2();
+  render3();
 
 }
-  render2();
+function resizer() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 }
+
+
 })(window || this);
--- vertex.glsl
+++ vertex.glsl
@@ -1,20 +1,18 @@
-precision mediump float;
-attribute vec4 aPosition;
+//precision mediump float;
 attribute vec2 vPosition;
 attribute vec3 vColor;
 varying vec3 fColor;
 uniform float theta;
 
 void main() {
+  fColor = vColor;
 
-    fColor = vColor;
-
-  mat4 rotate = mat4(
-    cos(theta), -sin(theta), 0.0, +0.5*cos(theta)-0.5,
-    sin(theta), cos(theta), 0.0, +0.5*sin(theta),
+  mat4 rotateM = mat4(
+    cos(theta), sin(theta), 0.0, +0.8*cos(theta) - 0.7,
+    -sin(theta), cos(theta), 0.0, -0.95*sin(theta) + -0.25,
     0.0, 0.0, 1.0, 0.0,
     0.0, 0.0, 0.0, 1.0
   );
 
-  gl_Position = vec4(vPosition, 0.0, 1.0) * rotate;
+  gl_Position = vec4(vPosition, 0.0, 1.0) * rotateM;
   }
