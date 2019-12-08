09fd64077ce3f69029c5e03e7d260d22a26ef260
jeremyvw <jeremyvw30@gmail.com>
2019-12-08 21:01:21 +0700

    Tambah variabel

--- fragment.glsl
+++ fragment.glsl
@@ -1,11 +1,15 @@
-// void main() {
-  
-//   gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
-// }
-
 precision mediump float;
 
+varying vec2 fTexCoord;
 varying vec3 fColor;
+varying vec3 fNormal;
+varying vec3 fPosition;
+
+uniform vec3 diffuseColor;
+uniform vec3 diffusePosition;
+uniform vec3 ambientColor;
+uniform int fFlag;
+
 
 void main() {
   gl_FragColor = vec4(fColor, 1.0);
